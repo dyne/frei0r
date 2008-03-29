845ddcdef0af540023b92317749c2a063c1ba37a
tailor <richard.spindler@gmail.com>
2008-03-29 19:09:15 +0000

    [project @ changed hsv vectorscope to CrCb vectorscope]

--- src/filter/vectorscope/vectorscope.c
+++ src/filter/vectorscope/vectorscope.c
@@ -1,5 +1,6 @@
 /* vectorscope.c
- * Copyright (C) 2007 Albert Frisch (albert.frisch@gmail.com)
+ * Copyright (C) 2008 Albert Frisch (albert.frisch AT gmail.com)
+ * Copyright (C) 2008 Richard Spindler (richard.spindler AT gmail.com)
  * This file is a Frei0r plugin.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -21,6 +22,7 @@
 #include <math.h>
 #include <assert.h>
 #include "frei0r.h"
+#include <stdio.h>
 
 #define OFFSET_R        0
 #define OFFSET_G        8
@@ -30,6 +32,10 @@
 #define GRID_COLOR	0xFF8E8E8E //144D7D
 #define SCOPE_COLOR	0xFFFF917F
 #define SCOPE_STEPS	10
+
+typedef struct {
+	double Y, Cb, Cr;
+} YCbCr_t;
 
 typedef struct {
 	double hue, sat, val;
@@ -49,6 +55,7 @@ typedef struct {
 
 typedef struct vectorscope_instance {
 	int w, h;
+	double scale;
 } vectorscope_instance_t;
 
 int f0r_init()
@@ -67,18 +74,29 @@ void f0r_get_plugin_info( f0r_plugin_info_t* info )
 	info->frei0r_version = FREI0R_MAJOR_VERSION;
 	info->major_version = 0; 
 	info->minor_version = 1; 
-	info->num_params =  0; 
+	info->num_params =  1; 
 	info->explanation = "Displays the vectorscope of the video-data";
 }
 
 void f0r_get_param_info( f0r_param_info_t* info, int param_index )
-{ /* empty */ }
+{
+
+	switch(param_index)
+	{
+		case 0:
+			info->name = "Scale";
+			info->type = F0R_PARAM_DOUBLE;
+			info->explanation = "";
+			break;
+	}
+}
 
 f0r_instance_t f0r_construct(unsigned int width, unsigned int height)
 {
 	vectorscope_instance_t* inst = (vectorscope_instance_t*)malloc(sizeof(vectorscope_instance_t));
 	inst->w = width;
 	inst->h = height;
+	inst->scale = 1.0;
 	return (f0r_instance_t)inst;
 }
 
@@ -89,10 +107,46 @@ void f0r_destruct(f0r_instance_t instance)
 }
 
 void f0r_get_param_value(f0r_instance_t instance, f0r_param_t param, int param_index)
-{ /* empty */ }
+{   
+	assert(instance);
+	vectorscope_instance_t* inst = (vectorscope_instance_t*)instance;
+	switch(param_index)
+	{
+		case 0:
+			*((double*)param) = (double)inst->scale;
+			break;
+	}
+}
 
 void f0r_set_param_value(f0r_instance_t instance, f0r_param_t param, int param_index)
-{ /* empty */ }
+{
+	assert(instance);
+	vectorscope_instance_t* inst = (vectorscope_instance_t*)instance;
+	switch(param_index)
+	{
+		int val;
+		case 0:
+		inst->scale = *((double*)param);
+		break;
+	}
+}
+
+ /* RGB to YCbCr range 0-255 */
+YCbCr_t rgb_to_YCbCr(rgb_t rgb)
+{
+	YCbCr_t dest;
+	//dest[0] = (float)0.257*data[0] + (float)0.504*data[1] + (float)0.098*data[2] + 16;
+	//dest[1] = (float)-0.148*data[0] - (float)0.291*data[1] + (float)0.439*data[2] + 128;
+	//dest[2] = (float)0.439*data[0] - (float)0.368*data[1] - (float)0.071*data[2] + 128;
+
+	dest.Y = (float)((0.299 * (float)rgb.red + 0.587 * (float)rgb.green + 0.114 * (float)rgb.blue));
+	dest.Cb = 128 + (float)((-0.16874 * (float)rgb.red - 0.33126 * (float)rgb.green + 0.5 * (float)rgb.blue));
+	dest.Cr = 128 + (float)((0.5 * (float)rgb.red - 0.41869 * (float)rgb.green - 0.08131 * (float)rgb.blue));
+
+
+	return dest;
+
+}
 
 hsv_t rgb_to_hsv(rgb_t rgb)
 {
@@ -192,15 +246,58 @@ void f0r_update(f0r_instance_t instance, double time, const uint32_t* inframe, u
 	vectorscope_instance_t* inst = (vectorscope_instance_t*)instance;
 
 	uint32_t* dst = outframe;
+	uint32_t* dst_end;
 	const uint32_t* src = inframe;
+	const uint32_t* src_end;
 
 	int width = inst->w;
 	int height = inst->h;	
 	int len = inst->w * inst->h;
 	
+	YCbCr_t YCbCr;
+	rgb_t rgb;
+	uint8_t* pixel;
+	int x, y;
+	dst_end = dst + len;
+	src_end = src + len;
+
+	while ( dst < dst_end ) {
+		*(dst++) = 0xFF000000;
+	}
+	dst = outframe;
+
+	double scale;
+	if ( width > height ) {
+		scale = height/(inst->scale);
+	} else {
+		scale = width/(inst->scale);
+	}
+	
+	while ( src < src_end ) {
+		rgb.red = (((*src) & 0x000000FF) >> OFFSET_R);
+		rgb.green = (((*src) & 0x0000FF00) >> OFFSET_G);
+		rgb.blue = (((*src) & 0x00FF0000) >> OFFSET_B);
+		src++;
+		YCbCr = rgb_to_YCbCr(rgb);
+		x = lrint((width/2)+((YCbCr.Cb/255.0-0.5) * scale));
+		y = lrint((height/2)-((YCbCr.Cr/255.0-0.5) * scale));
+		//printf ("Cb: %d, Cr: %d\n", x, y );
+		if ( x >= 0 && x < width && y >= 0 && y < height ) {
+			pixel = (uint8_t*)&dst[x+width*y];
+			if ( pixel[0] < 240 ) {
+				pixel[0]+=10;
+				pixel[1]+=10;
+				pixel[2]+=10;
+			}
+			//dst[x+width*y] += 1;//0xFFFFFFFF;
+		}
+	}
+
+	
+/*
+
 	long i,j;
-	rgb_t rgb, pixel;
-	hsv_t hsv;
+	//hsv_t hsv;
 	coord_pol_t pol;
 	coord_rect_t rect;
 	long offset;
@@ -215,9 +312,12 @@ void f0r_update(f0r_instance_t instance, double time, const uint32_t* inframe, u
 		rgb.blue = (((*src) & 0x00FF0000) >> OFFSET_B);
 		src++;
 
-		hsv = rgb_to_hsv(rgb);
-		pol.phi = hsv.hue/180*M_PI;
-		pol.r = hsv.sat/100;
+		YCbCr = rgb_to_YCbCr(rgb);
+		//hsv = rgb_to_hsv(rgb);
+		//pol.phi = hsv.hue/180*M_PI;
+		//pol.r = hsv.sat/100;
+		pol.phi = YCbCr.hue/180*M_PI;
+		pol.r = YCbCr.sat/100;
 		offset = pixel_offset(pol, width, height);
 		if ( offset < len ) {
 			scope[offset]++;
@@ -239,5 +339,6 @@ void f0r_update(f0r_instance_t instance, double time, const uint32_t* inframe, u
 			dst[i] = 0xFF000000 | ((int)pixel.blue<<OFFSET_B) | ((int)pixel.green<<OFFSET_G) | ((int)pixel.red<<OFFSET_R);
 		}
 	}
+	*/
 }
 
