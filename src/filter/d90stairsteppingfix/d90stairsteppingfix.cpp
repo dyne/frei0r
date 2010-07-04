afdda6af3f993b79aa07781b413d1616a33fe1bc
Simon Eugster <simon.eu@gmail.com>
2010-07-05 00:28:19 +0200

    not working yet.

--- src/filter/d90stairsteppingfix/d90stairsteppingfix.cpp
+++ src/filter/d90stairsteppingfix/d90stairsteppingfix.cpp
@@ -1,26 +1,124 @@
 #include "frei0r.hpp"
 
 #include <stdio.h>
+#include <math.h>
 
 static int slices[] = {7,9,9,8,9,9,9,9,9,8,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,
                     9,8,9,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,8,9,9,9,9,9,8,
                     9,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,8,9,9,
                     9,9,9,8,9,9,7};
 
+struct rgbColor
+{
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
+  unsigned char a;
+} test;
+
 class D90StairsteppingFix : public frei0r::filter
 {
 
 public:
-    
-    
     D90StairsteppingFix(unsigned int width, unsigned int height)
     {
+        m_interpolationValues = new float[height];
+        
+        
+        //printf("Struct size: %d. Pixel size: %d.\n", sizeof(rgbColor), sizeof(uint32_t));
+        
+        if (height == 720) {
+            
+            int sliceNumber = (sizeof slices)/(sizeof slices[0]);
+            int newSize = height + sliceNumber;
+            
+            printf("%d slices, %d total new lines\n", sliceNumber, newSize);
+            
+            float withInsertedLines[newSize];
+            
+            int count = 0;
+            int index = 0;
+            for (int i = 0; i < sliceNumber; i++) {
+                for (int j = 0; j < slices[i]; j++) {
+                    
+                    withInsertedLines[index] = count;
+                    count++;
+                    index++;
+                    
+                }
+                if (count < newSize) {
+                    withInsertedLines[index] = count - 0.5;
+                    index++;
+                }
+            }
+            for (int i = 0; i < newSize; i++) {
+                printf("inserted Lines: %f at %d\n", withInsertedLines[i], i);
+            }
+            
+            float interpolationWeights[height];
+            float scaleFactor = (float) newSize/height;
+            printf("scale factor: %f\n", scaleFactor);
+            for (int i = 0; i < height; i++) {
+                interpolationWeights[i] = i*scaleFactor;
+            }
+            for (int i = 0; i < height; i++) {
+                printf("scaled: %f at %d\n", interpolationWeights[i], i);
+            }
+            
+            float offset;
+            for (int i = 0; i < height; i++) {
+                
+                index = floor(interpolationWeights[i]);
+                offset = interpolationWeights[i] - index;
+                
+                if (i+1 < height) {
+                    m_interpolationValues[i] = offset*interpolationWeights[i] + (1-offset)*interpolationWeights[i+1];
+                } else {
+                    m_interpolationValues[i] = interpolationWeights[i];
+                    if (m_interpolationValues[i] > (newSize-1)) {
+                        m_interpolationValues[i] = newSize-1;
+                    }
+                }
+                printf("%f at %d with weights %f and %f\n", m_interpolationValues[i], 
+                    i, offset*interpolationWeights[i], (1-offset)*interpolationWeights[i+1]);
+            }
+            
+        } else {
+            // Not a 720p file.
+        }
+    }
+    
+    ~D90StairsteppingFix()
+    {
+        delete m_interpolationValues;
     }
 
     virtual void update()
     {
-        std::copy(in, in + width*height, out);
+        /*struct rgbColor* col = (struct rgbColor*) &in[0];*/
+        struct rgbColor col = *(struct rgbColor*) &in[0];
+        
+        printf("Color value of first pixel: r=%d g=%d b=%d a=%d.\n", col.r, col.g, col.b, col.a);
+        if (height == 720) {
+            printf("Converting.\n");
+            int factor;
+            
+            for (int line = 0; line+1 < height; line++) {
+                factor = m_interpolationValues[line] - floor(m_interpolationValues[line]);
+                for (int index = 0; index < width; index++) {
+                    out[width*line+index] = factor*in[width*line+index] + (1-factor) * in[width*(line+1) + index];
+                    if (index % 2 == 0) { out[width*line+index] = 0; }
+                }
+            }
+            std::copy(in + width*(height-1), in+width*height, out + width*(height-1));
+        } else {
+            printf("Just copying. Height is %d.\n", height);
+            std::copy(in, in + width*height, out);
+        }
     }
+    
+private:
+    float *m_interpolationValues;
 
 };
 
@@ -29,4 +127,5 @@ public:
 frei0r::construct<D90StairsteppingFix> plugin("Nikon D90 Stairstepping fix",
                 "Removes the Stairstepping from Nikon D90 videos by interpolation",
                 "Simon A. Eugster (Granjow)",
-                0,1);
+                0,1,
+                F0R_COLOR_MODEL_RGBA8888);
