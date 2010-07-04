1cfb3077fe265e0152aa76db6c4e272eaf7754e8
Jaromil <jaromil@dyne.org>
2010-07-04 17:25:58 +0200

    fixed in post-hackmeeting with anarchia

--- src/filter/facebl0r/facebl0r.cpp
+++ src/filter/facebl0r/facebl0r.cpp
@@ -35,18 +35,6 @@ typedef struct {
   CvBox2D curr_box;  //current face location estimate
 } TrackedObj;
 
-// camshift
-TrackedObj* create_tracked_object (IplImage* image, CvRect* face_rect);
-void destroy_tracked_object (TrackedObj* tracked_obj);
-CvBox2D camshift_track_face (IplImage* image, TrackedObj* imgs);
-void update_hue_image (const IplImage* image, TrackedObj* imgs);
-
-//trackface
-IplImage* capture_video_frame (CvCapture*);
-CvRect* detect_face (IplImage*, CvHaarClassifierCascade*, CvMemStorage*);
-void cleanup (char*, CvHaarClassifierCascade*, CvMemStorage*);
-void print_help (char*);
-
 
 
 class FaceBl0r: public frei0r::filter {
@@ -59,7 +47,15 @@ public:
 
 private:
     
-    void _init(int wdt, int hgt);
+// camshift
+    TrackedObj* create_tracked_object (IplImage* image, CvRect* face_rect);
+    void destroy_tracked_object (TrackedObj* tracked_obj);
+    CvBox2D camshift_track_face (IplImage* image, TrackedObj* imgs);
+    void update_hue_image (const IplImage* image, TrackedObj* imgs);
+    
+//trackface
+    CvRect* detect_face (IplImage*, CvHaarClassifierCascade*, CvMemStorage*);
+    
 
     TrackedObj* tracked_obj;
     CvBox2D face_box; //area to draw
@@ -73,7 +69,7 @@ private:
 
     char *classifier;
 
-    int bytesize;
+    bool face_found;
 };
 
 
@@ -83,30 +79,31 @@ frei0r::construct<FaceBl0r> plugin("FaceBl0r",
 				  1,0);
 
 FaceBl0r::FaceBl0r(int wdt, int hgt) {
-  CvHaarClassifierCascade* cascade = 0;
-  CvMemStorage* storage = 0;
 
   width = wdt;
   height = hgt;
-  bytesize = width*height*4;
+  size = width*height*4;
   //initialize
   classifier = (const char*)"/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml";
   cascade = (CvHaarClassifierCascade*) cvLoad(classifier, 0, 0, 0 );
   storage = cvCreateMemStorage(0);
   //validate
-  //assert(cascade && storage);
+  assert(cascade && storage);
 
   face_rect = 0;
   image = 0;
   tracked_obj = 0;
+  face_found = false;
 
 }
 
 FaceBl0r::~FaceBl0r() {
     if(tracked_obj)
         destroy_tracked_object(tracked_obj);
-//    cleanup(cascade);
-//    cleanup(storage);
+
+//    if(cascade) cvReleaseHaarClassifierCascade(&cascade);
+//    if(storage) cvReleaseMemStorage(&storage);
+    
 }
 
 void FaceBl0r::update() {
@@ -115,49 +112,110 @@ void FaceBl0r::update() {
 
   if( !image )
       image = cvCreateImage( cvSize(width,height), IPL_DEPTH_8U, 4 );
+
   unsigned char* ipli = (unsigned char*)image->imageData;
   int step = image->widthStep;
   unsigned i, j;
   for (i = 0; (i < height); i++) {
     for (j = 0; (j < width); j++) {
-      ipli[i*step+j*4+2] = src[2];
-      ipli[i*step+j*4+1] = src[1];
-      ipli[i*step+j*4+0] = src[0];
+
+//        ipli[(i*step)+j*4+0] = src[0];
+        ipli[(i*step)+j*4+2] = src[2];
+        ipli[(i*step)+j*4+1] = src[1];
+        ipli[(i*step)+j*4+0] = src[0];
       //ipli += 4;
       src += 4;
     }
   }
 
-  face_rect = detect_face(image, cascade, storage);
-  // if face detected
-  if (!face_rect) {
-      memcpy(out,in,bytesize);
-      return;
+
+  /*
+    no face*
+     - look for (detect_face)
+    yes face
+     - track face
+     - no more face
+       no face*
+   */
+
+  if(!face_found) {
+      face_rect = detect_face(image, cascade, storage);
+      // if face detected
+      if (!face_rect) {
+          memcpy(out,in,size);
+          return;
+      }
+      //track detected face with camshift
+      tracked_obj = create_tracked_object(image, face_rect);
+      face_found = true;
   }
-
-  //track detected face with camshift
-  tracked_obj = create_tracked_object(image, face_rect);
-
 
   //track the face in the new frame
   face_box = camshift_track_face(image, tracked_obj);
-  
+
+  if( ( face_box.size.width < 10 )
+      || (face_box.size.height <10 )
+      || (face_box.size.width > 500 )
+      || (face_box.size.height > 500 )
+      ) {
+
+      face_found = false;
+      return;
+  }
+
 ////////////////////////////////////////////////////////////////////////
   cvSetImageROI (image, tracked_obj->prev_rect);
-  cvSmooth (image, image, CV_BLUR, 22, 22, 0, 0);
-//      cvSmooth (image, image, CV_BLUR, 11, 11, 0, 0);
+//  cvSmooth (image, image, CV_BLUR, 22, 22, 0, 0);
+      cvSmooth (image, image, CV_BLUR, 11, 11, 0, 0);
 //      cvSmooth (image, image, CV_GAUSSIAN, 11, 11, 0, 0);
   cvResetImageROI (image);
 ////////////////////////////////////////////////////////////////////////
       
   //outline face ellipse
   cvEllipseBox(image, face_box, CV_RGB(255,0,0), 3, CV_AA, 0);
-  memcpy(out,image->imageData,bytesize);
+
+//  memcpy(out,image->imageData,size);
+  src = (unsigned char *)in;
+
+  for (i = 0; (i < height); i++) {
+    for (j = 0; (j < width); j++) {
+
+//        ipli[(i*step)+j*4+0] = src[0];
+        dst[3] = src[3];
+        dst[2] = ipli[(i*step)+j*4+2];
+        dst[1] = ipli[(i*step)+j*4+1];
+        dst[0] = ipli[(i*step)+j*4+0];
+      //ipli += 4;
+        dst += 4;
+        src += 4;
+    }
+  }
 
 }
 
+/* Given an image and a classider, detect and return region. */
+CvRect* FaceBl0r::detect_face (IplImage* image,
+                               CvHaarClassifierCascade* cascade,
+                               CvMemStorage* storage) {
+    
+  CvRect* rect = 0;
+  
+  //get a sequence of faces in image
+  CvSeq *faces = cvHaarDetectObjects(image, cascade, storage,
+     1.1,                       //increase search scale by 10% each pass
+     6,                         //require 6 neighbors
+     CV_HAAR_DO_CANNY_PRUNING,  //skip regions unlikely to contain a face
+     cvSize(0, 0));             //use default face size from xml
+
+  //if one or more faces are detected, return the first one
+  if(faces && faces->total)
+    rect = (CvRect*) cvGetSeqElem(faces, 0);
+
+  return rect;
+}
+
 /* Create a camshift tracked object from a region in image. */
-TrackedObj* create_tracked_object (IplImage* image, CvRect* region) {
+TrackedObj* FaceBl0r::create_tracked_object (IplImage* image, CvRect* region) {
   TrackedObj* obj;
   
   //allocate memory for tracked object struct
@@ -200,7 +258,7 @@ TrackedObj* create_tracked_object (IplImage* image, CvRect* region) {
 }
 
 /* Release resources from tracked object. */
-void destroy_tracked_object (TrackedObj* obj) {
+void FaceBl0r::destroy_tracked_object (TrackedObj* obj) {
   cvReleaseImage(&obj->hsv);
   cvReleaseImage(&obj->hue);
   cvReleaseImage(&obj->mask);
@@ -211,7 +269,7 @@ void destroy_tracked_object (TrackedObj* obj) {
 }
 
 /* Given an image and tracked object, return box position. */
-CvBox2D camshift_track_face (IplImage* image, TrackedObj* obj) {
+CvBox2D FaceBl0r::camshift_track_face (IplImage* image, TrackedObj* obj) {
   CvConnectedComp components;
 
   //create a new hue image
@@ -233,7 +291,7 @@ CvBox2D camshift_track_face (IplImage* image, TrackedObj* obj) {
   return obj->curr_box;
 }
 
-void update_hue_image (const IplImage* image, TrackedObj* obj) {
+void FaceBl0r::update_hue_image (const IplImage* image, TrackedObj* obj) {
   //limits for calculating hue
   int vmin = 65, vmax = 256, smin = 55;
   
