8218dee35d3b2ede8b8bbd8785125094e8210794
Simon Eugster <simon.eu@gmail.com>
2010-07-06 17:36:03 +0200

    Use pointer arithmetics instead of struct. Speed gain: 20 %.

--- src/filter/d90stairsteppingfix/d90stairsteppingfix.cpp
+++ src/filter/d90stairsteppingfix/d90stairsteppingfix.cpp
@@ -110,13 +110,6 @@ static int slices720p[] = {7,9,9,8,9,9,9,9,9,8,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,
                     9,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,9,8,9,9,9,9,8,9,9,
                     9,9,9,8,9,9,7};
 
-struct rgbColor
-{
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
-  unsigned char a;
-} test;
 
 class D90StairsteppingFix : public frei0r::filter
 {
@@ -125,9 +118,6 @@ public:
     D90StairsteppingFix(unsigned int width, unsigned int height)
     {
         m_mesh = new float[height];
-        
-        
-//      printf("Struct size: %d. Pixel size: %d.\n", sizeof(rgbColor), sizeof(uint32_t));
         
         if (height == 720) {
             
@@ -204,44 +194,17 @@ public:
     {
         delete m_mesh;
     }
-    
-    struct rgbColor toRGB(uint32_t input)
-    {
-        struct rgbColor rgbCol;
-        rgbCol.r = (input & 0x000000FF) >> 0;
-        rgbCol.g = (input & 0x0000FF00) >> 8;
-        rgbCol.b = (input & 0x00FF0000) >> 16;
-        rgbCol.a = (input & 0xFF000000) >> 24;
-        return rgbCol;
-    }
-    uint32_t toUint(struct rgbColor rgbCol)
-    {
-        uint32_t pixel = 0;
-        pixel = pixel | (rgbCol.r << 0);
-        pixel = pixel | (rgbCol.g << 8);
-        pixel = pixel | (rgbCol.b << 16);
-        pixel = pixel | (rgbCol.a << 24);
-        return pixel;
-    }
 
     virtual void update()
     {
-        // This does not need to work, it just may. 
-        // So use the toRGB/toUint from above.
-        /*struct rgbColor* col = (struct rgbColor*) &in[0];*/
-        /*struct rgbColor col = *(struct rgbColor*) &in[0];*/
-        
-        struct rgbColor col, colA, colB;
-        
-//      col = toRGB(in[0]);
-//      printf("Color value of first pixel: r=%d g=%d b=%d a=%d.\nuint value: %d.\n", col.r, col.g, col.b, col.a, in[0]);
 
         if (height == 720) {
 //          printf("Converting.\n");
 
             float factor;
             int index;
-            unsigned char r,g,b,a;
+            unsigned char *cvA, *cvB, *cvOut;
+            
             
             for (int line = 0; line < height; line++) {
                 index = floor(m_mesh[line]);
@@ -249,20 +212,26 @@ public:
                 
                 for (int pixel = 0; pixel < width; pixel++) {
                     // Use linear interpolation on the colours
-                
-                    colA = toRGB(in[width*index+pixel]);
-                    colB = toRGB(in[width*(index+1) + pixel]);
                     
-                    col.r = floor((float)(1-factor)*colA.r + (factor)*colB.r);
-                    col.g = floor((float)(1-factor)*colA.g + (factor)*colB.g);
-                    col.b = floor((float)(1-factor)*colA.b + (factor)*colB.b);
-                    col.a = floor((float)(1-factor)*colA.a + (factor)*colB.a);
+                    // Use pointer arithmetics. Colour values are stored 
+                    // as AABBGGRR in the uint32_t values.
+                    // Convert each colour separately.
                     
-                    out[width*line+pixel] = toUint(col);
+                    cvA = (unsigned char*) &in[width*index+pixel];
+                    cvB = (unsigned char*) &in[width*(index+1) + pixel];
+                    cvOut = (unsigned char*) &out[width*line+pixel];
+                    
+                    for (unsigned char i = 0; i < 4; i++) {
+                        *cvOut = floor((float)(1-factor)*(*cvA) + (factor)*(*cvB));
+                        cvA++;
+                        cvB++;
+                        cvOut++;
+                    }
                 }
             }
             std::copy(in + width*(height-1), in+width*height, out + width*(height-1));
         } else {
+            // Not a 720p file -> Cannot work, do nothing.
 //          printf("Just copying. Height is %d.\n", height);
             std::copy(in, in + width*height, out);
         }
@@ -276,7 +245,7 @@ private:
 
 
 frei0r::construct<D90StairsteppingFix> plugin("Nikon D90 Stairstepping fix",
-                "Removes the Stairstepping from Nikon D90 videos by interpolation",
+                "Removes the Stairstepping from Nikon D90 videos (720p only) by interpolation",
                 "Simon A. Eugster (Granjow)",
                 0,1,
                 F0R_COLOR_MODEL_RGBA8888);
