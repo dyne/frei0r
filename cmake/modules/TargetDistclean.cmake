5d825c423fd21540d5b3cf1b7890f0c201f0abdd
Jaromil <jaromil@dyne.org>
2010-09-13 11:59:40 +0200

    added distclean target to cmake for better cleanup when making releases also added cmake build files in .gitignore

---
+++ cmake/modules/TargetDistclean.cmake
@@ -0,0 +1,28 @@
+# add custom target distclean
+# cleans and removes cmake generated files etc.
+# Jan Woetzel 04/2003
+#
+
+IF (UNIX)
+  ADD_CUSTOM_TARGET (distclean @echo cleaning for source distribution)
+  SET(DISTCLEANED
+   cmake.depends
+   cmake.check_depends
+   CMakeCache.txt
+   cmake.check_cache
+   *.cmake
+   Makefile
+   core core.*
+   gmon.out
+   *~
+  )
+  
+  ADD_CUSTOM_COMMAND(
+    DEPENDS clean
+    COMMENT "distribution clean"
+    COMMAND rm
+    ARGS    -Rf CMakeTmp ${DISTCLEANED}
+    TARGET  distclean
+  )
+ENDIF(UNIX)
+
