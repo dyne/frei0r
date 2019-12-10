b27f03c8d51c34cd5f79e0399bb0024ca94ea813
IOhannes m zmölnig <zmoelnig@iem.at>
2019-12-10 15:01:52 +0100

    facedetect: include imgproc/imgproc_c.h when using OpenCV-4+
    imgproc_c.h defines the constants CV_BGR2GRAY, CV_FILLED & CV_AA.
    
    it also includes "core/core_c.h", which then provides
      `cvGetTickCount()` and `cvGetTickFrequency()`

--- src/filter/facedetect/facedetect.cpp
+++ src/filter/facedetect/facedetect.cpp
@@ -21,6 +21,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <opencv2/opencv.hpp>
+#if CV_MAJOR_VERSION >= 4
+# include <opencv2/imgproc/imgproc_c.h>
+#endif
 #include "frei0r.hpp"
 #include "frei0r_math.h"
 
