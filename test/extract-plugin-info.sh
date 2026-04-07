a2347110c888d5c8a133f95325482c7bec54e5cd
Jaromil <jaromil@dyne.org>
2026-04-07 23:45:08 +0200

    fix(test): use frei0r-meta in extract script

--- test/extract-plugin-info.sh
+++ test/extract-plugin-info.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-[ -r frei0r-info ] || gmake
+[ -r frei0r-meta ] || gmake
 tmp=`mktemp`
 find "${2}" -name "*.$1" |
     while read -r line; do
 		file=`basename $line`
 		dir=`dirname $line`
 		name=`echo $file | cut -d. -f1`
-		./frei0r-info "$line" > $tmp
+		./frei0r-meta "$line" > $tmp
 		mv $tmp "$dir/$name.json"
 		ls "$dir/$name.json"
 	done
