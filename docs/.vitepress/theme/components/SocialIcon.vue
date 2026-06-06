560b0690ff58985617f026782e2ce29a260eb4e9
Jaromil <jaromil@dyne.org>
2026-06-06 22:56:15 +0200

    feat(website): install Dyne VitePress scaffold

---
+++ docs/.vitepress/theme/components/SocialIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    class="dyne-social-icon"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <g v-html="icon"></g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { socialIcons, type SocialIconName } from './social-icons'
+
+const props = defineProps<{
+  name: SocialIconName
+}>()
+
+const icon = computed(() => socialIcons[props.name])
+</script>
